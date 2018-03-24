1259dc97f1b2a2eca54a6e09d68d0250529337d2
Jihye Eom <>
2018-03-24 17:58:05 +0900

    fixed errors

---
+++ src/Droid/appcenter-pre-build.sh
@@ -0,0 +1,3 @@
+﻿#!/usr/bin/env bash
+
+sed -i -e "s/\[VSAC_APIKEY]/$VSAC_Key/g" ../Constants.cs 
---
+++ src/appcenter-pre-build.sh
@@ -0,0 +1,7 @@
+﻿#!/usr/bin/env bash
+if [ "$APPCENTER_BRANCH" == "QA" ] ;
+then
+/usr/libexec/plistbuddy -c "Set :CFBundleDisplayName VSACXamarin.$APPCENTER_BRANCH" "iOS/Info.plistbuddy"
+fi
+
+sed -i -e "s/\[VSAC_APIKEY]/$VSAC_Key/g" Constants.cs 
